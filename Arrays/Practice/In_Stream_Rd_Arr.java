0dd6513fbdc21a6e5efa452d2d3300fd392b0b09
Rish <akshaw05@aol.com>
2022-12-19 22:39:33 +0530

    Array

--- Arrays/Practice/In_Stream_Rd_Arr.java
+++ Arrays/Practice/In_Stream_Rd_Arr.java
@@ -7,6 +7,17 @@ public class In_Stream_Rd_Arr
     public static void main(String[]args) throws IOException
     {
         InputStreamReader read = new InputStreamReader(System.in);
-        
+        BufferedReader in = new BufferedReader(read);
+        int i,sum=0;
+        int m[] = new int[10];
+        for (i = 0; i < 10; i++){
+            System.out.println("Enter the marks of the students :"+i);
+                m[i] = Integer.parseInt(in.readLine());
+        }
+        for(i=0;i<10;i++){
+            sum = sum+m[i];
+           // System.out.println("The sum of the array elements = "+sum);
+        }
+        System.out.println("The sum of the array elements = "+sum);
     }
 }
