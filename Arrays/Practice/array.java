4ca17098f0ea22a9d7ae16f9a519f3d1ac5ab6c0
Rish <akshaw05@aol.com>
2022-12-20 13:21:35 +0530

    Array

---
+++ Arrays/Practice/array.java
@@ -0,0 +1,15 @@
+package Practice;
+
+public class array {
+    public static void main(String[] args) {
+        int[] a={46,54},b={56,4};
+      
+        System.out.print(b[0]+" ");
+        System.out.print(b[1]+"\n");
+        System.out.println(a[0]+" "+a[1]);
+
+        int[][] L = new int[2][3];
+        L[0][0]=5;
+        System.out.println(L[0][0]);
+    }
+}
